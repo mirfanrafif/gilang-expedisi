2947dfc67f3668bdd7f9825fcfe7c8ec2f51599c
Irfan Rafif <mirfanrafif17@gmail.com>
2022-05-22 00:01:16 +0700

    dialog verifikasi timbang

--- lib/components/pages/daftar_timbang_page.dart
+++ lib/components/pages/daftar_timbang_page.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
+
 import 'package:aplikasi_timbang/bloc/timbang/timbang_bloc.dart';
 import 'package:aplikasi_timbang/bloc/timbang/timbang_state.dart';
+import 'package:aplikasi_timbang/components/pages/menu_page.dart';
 import 'package:aplikasi_timbang/components/widgets/hasil_timbang.dart';
 import 'package:aplikasi_timbang/data/models/timbang.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 
-class DaftarTimbangPage extends StatelessWidget {
+class DaftarTimbangPage extends StatefulWidget {
   const DaftarTimbangPage({Key? key}) : super(key: key);
 
+  @override
+  State<DaftarTimbangPage> createState() => _DaftarTimbangPageState();
+}
+
+class _DaftarTimbangPageState extends State<DaftarTimbangPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,7 +102,7 @@ class DaftarTimbangPage extends StatelessWidget {
                 width: double.infinity,
                 height: 40,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: getVerifikasiDialog,
                   child: const Text("Simpan dan Verifikasi Hasil Timbang"),
                 ),
               )
@@ -102,5 +111,137 @@ class DaftarTimbangPage extends StatelessWidget {
         );
       }),
     );
+  }
+
+  void getVerifikasiDialog() {
+    showDialog(
+        context: context,
+        builder: (context) => Dialog(
+              insetPadding: const EdgeInsets.all(16),
+              child: StatefulBuilder(builder: (context, setState) {
+                return Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Verifikasi Petugas Kandang",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      const Text("Kirim foto data timbang"),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          final imagePicker = ImagePicker();
+                          var result = await imagePicker.pickImage(
+                              source: ImageSource.camera);
+
+                          if (result != null) {
+                            setState(() {
+                              buktiTimbang = File(result.path);
+                            });
+                          }
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          height: 180,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black12,
+                              width: 1,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: buktiTimbang != null
+                              ? Image(
+                                  image: FileImage(buktiTimbang!),
+                                  fit: BoxFit.cover,
+                                )
+                              : Center(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: const [
+                                      Icon(Icons.camera),
+                                      Text("Tekan disini untuk mengambil foto"),
+                                    ],
+                                  ),
+                                ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 40,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            tampilkanDialogSukses();
+                          },
+                          child: const Text("Simpan dan Kirim Hasil Timbang"),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }),
+            ));
+  }
+
+  File? buktiTimbang;
+
+  void tampilkanDialogSukses() {
+    showDialog(
+        context: context,
+        builder: (context) => Dialog(
+              insetPadding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Verifikasi Berhasil",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MenuPage(),
+                              ),
+                              (route) => false);
+                        },
+                        child: const Text("Kembali ke Menu Utama"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ));
   }
 }
